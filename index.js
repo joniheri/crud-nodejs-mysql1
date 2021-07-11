e8838ce62f3b40565976230a71bf689cf980b266
Rizky Tanjung <kimsohyuncans@gmail.com>
2021-07-11 14:32:33 +0700

    CRUD Version 1 with ExpressJS OK

--- index.js
+++ index.js
@@ -1,78 +1,94 @@
 //use path module
-const path = require('path');
+const path = require("path");
+
 //use express module
-const express = require('express');
+const express = require("express");
+
 //use hbs view engine
-const hbs = require('hbs');
+const hbs = require("hbs");
+
 //use bodyParser middleware
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
+
 //use mysql database
-const mysql = require('mysql');
+const mysql = require("mysql");
 const app = express();
 
 //Create Connection
 const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'crud_db'
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "crud_db",
 });
 
 //connect to database
-conn.connect((err) =>{
-  if(err) throw err;
-  console.log('Mysql Connected...');
+conn.connect((err) => {
+  if (err) throw err;
+  console.log("Mysql Connected...");
 });
 
 //set views file
-app.set('views',path.join(__dirname,'views'));
+app.set("views", path.join(__dirname, "views"));
+
 //set view engine
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
 //set folder public as static folder for static file
-app.use('/assets',express.static(__dirname + '/public'));
+app.use("/assets", express.static(__dirname + "/public"));
 
 //route for homepage
-app.get('/',(req, res) => {
+app.get("/", (req, res) => {
   let sql = "SELECT * FROM product";
   let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-    res.render('product_view',{
-      results: results
+    if (err) throw err;
+    res.render("product_view", {
+      results: results,
     });
   });
 });
 
 //route for insert data
-app.post('/save',(req, res) => {
-  let data = {product_name: req.body.product_name, product_price: req.body.product_price};
+app.post("/save", (req, res) => {
+  let data = {
+    product_name: req.body.product_name,
+    product_price: req.body.product_price,
+  };
+  // console.log(data);
   let sql = "INSERT INTO product SET ?";
-  let query = conn.query(sql, data,(err, results) => {
-    if(err) throw err;
-    res.redirect('/');
+  let query = conn.query(sql, data, (err, results) => {
+    if (err) throw err;
+    res.redirect("/");
   });
 });
 
 //route for update data
-app.post('/update',(req, res) => {
-  let sql = "UPDATE product SET product_name='"+req.body.product_name+"', product_price='"+req.body.product_price+"' WHERE product_id="+req.body.id;
+app.post("/update", (req, res) => {
+  let sql =
+    "UPDATE product SET product_name='" +
+    req.body.product_name +
+    "', product_price='" +
+    req.body.product_price +
+    "' WHERE product_id=" +
+    req.body.id;
   let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-    res.redirect('/');
+    if (err) throw err;
+    res.redirect("/");
   });
 });
 
 //route for delete data
-app.post('/delete',(req, res) => {
-  let sql = "DELETE FROM product WHERE product_id="+req.body.product_id+"";
+app.post("/delete", (req, res) => {
+  let sql = "DELETE FROM product WHERE product_id=" + req.body.product_id + "";
   let query = conn.query(sql, (err, results) => {
-    if(err) throw err;
-      res.redirect('/');
+    if (err) throw err;
+    res.redirect("/");
   });
 });
 
 //server listening
-app.listen(8000, () => {
-  console.log('Server is running at port 8000');
+app.listen(8009, () => {
+  console.log("Server is running at port 8000");
 });
